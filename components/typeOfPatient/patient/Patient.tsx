4e807c6fc39186985cb397c8bd0d1773d8bbd5fe
e-n-eliseev <e.n.eliseev@mail.ru>
2022-07-12 17:21:09 +0300

    done desktop

--- components/typeOfPatient/patient/Patient.tsx
+++ components/typeOfPatient/patient/Patient.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import { IPatient } from "../../../types/types";
 import uniqid from "uniqid";
 
-const Patient: FC<IPatient> = ({ name, img1, img2, description }) => {
+const Patient: FC<IPatient> = ({ name, img1, description }) => {
     return (
         <article className="patient">
             <h3 className="patient__heading">{name}</h3>
--- types/types.ts
+++ types/types.ts
@@ -1,15 +1,18 @@
 
-export interface IImg {
+export interface IBase {
     name: string;
     img1: string;
+}
+export interface IImg extends IBase {
     img2: string;
 }
 
-export interface IPatient extends IImg {
+export interface IPatient extends IBase {
     description: string[]
 }
 
 export interface ISymptom extends IPatient {
+    img2: string;
     angle: number
 }
 
